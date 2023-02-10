1b3c4a39394248467b9f128f168c9a5e1de9574a
AdilAhmed976 <adilahmedah976@gmail.com>
2023-02-10 22:05:24 +0530

    added responsiveness

---
+++ src/components/Logo.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+export const Icon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="52"
+      height="48"
+      fill="none"
+      viewBox="0 0 52 48"
+    >
+      <rect
+        width="11.048"
+        height="27.843"
+        fill="#3766E8"
+        rx="5.524"
+        transform="matrix(.85444 -.51955 .51053 .85986 .5 24.059)"
+      ></rect>
+      <rect
+        width="11.048"
+        height="42.369"
+        fill="#3766E8"
+        rx="5.524"
+        transform="matrix(.85444 -.51955 .51053 .85986 12.39 8.652)"
+      ></rect>
+      <rect
+        width="11.048"
+        height="21.548"
+        fill="#3766E8"
+        rx="5.524"
+        transform="matrix(.85444 -.51955 .51053 .85986 31.06 5.738)"
+      ></rect>
+    </svg>
+  );
+}
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../css/login.css';
+import { Icon } from '../components/Logo';
 export const Login = () => {
   return (
 
@@ -13,10 +14,43 @@ export const Login = () => {
 
         <div className='form_container' >
             <div className='form_container_box' >
-                <div>
+                <div className='form_container_box_logo' >
                     <img src="https://wobot.ai/wobot_logo_blue.svg" alt="logo" srcset="" />
                 </div>
-                <div></div>
+                <div className='form_container_box_form' >
+                        <div className='form' > 
+                            <div>
+                                <Icon/>
+                            </div>
+                            <div>
+                                <text >
+                                    It's a delight to have you onboard
+                                </text>
+                            </div>
+                            <div>
+                                <text >
+                                    Help us to know you better.
+                                </text>
+                            </div>
+                            <div>
+                                <text >
+                                    (This is how we optimize Wobot as per your business needs)
+                                </text>
+                            </div>
+                            <div className='boxes' >
+                                <label>Your Name</label>
+                                <input placeholder='e.g John Doe' />    
+                            </div>
+                            <div className='boxes' >
+                                <label>Your Name</label>
+                                <input placeholder='e.g John Doe' />    
+                            </div>
+                            <div className='boxes' >
+                                <label>Your Name</label>
+                                <input placeholder='e.g John Doe' />    
+                            </div>
+                        </div>
+                </div>
             </div>
         </div>
     </div>
